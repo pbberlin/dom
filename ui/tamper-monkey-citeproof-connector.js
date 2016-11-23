5cea087319bb8ce9e3b745ba4da5e8ffcd411809
pbberlin <peter.buchmann@web.de>
2016-11-23 19:54:58 +0100

    tamper monkey working

--- ui/tamper-monkey-citeproof-connector.js
+++ ui/tamper-monkey-citeproof-connector.js
@@ -7,10 +7,10 @@
 // @author       iche
 // @downloadURL
 // @updateURL
-// @match        *://*.welt.de/*
-// @match        *://citeproof.appspot.com/*
-// @match        *://*.economist.com/*
+// @match        *://*/*
+// @match        *://localhost:*/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.js
 // @grant        none
 // @noframes
 // @run-at      document-end
@@ -25,44 +25,76 @@ if (typeof jQuery === 'undefined') {
 
 (function ($, undefined) {
     $(function () {
-
         console.log("isolated jQuery start");
 
-$( document ).ready(function() {
 
-    console.log( "tamper monkey document ready start" );
-
-    var hidingCountdownID = 0;
-
-    console.log("tampermonk: window name is",window.name, "val is", window.val);
-    if (window.name==="ExcelDB_window") {
-        console.log("active");
-    }
+        function TriggerIt(){
+            // This only works, if second argument matches opener.window.href
+            // opener.postMessage("url-info", "http://localhost:8080");
+            var data = {type: "url-info", info: { loc: window.location.href, remoteUrl: "aaa" }};
 
 
-    $( 'a' ).on( "mouseenter", function(evt) {
-    });
-
-    $( 'a' ).on( "mouseleave", function(evt) {
-    });
-
-
-    $( '#popup1' ).on( "mouseenter", function(evt) {
-    });
-
-    $( '#popup1' ).on( "mouseleave", function(evt) {
-    });
+            // http://stackoverflow.com/questions/2120060
+            var content = document.body.parentNode.innerHTML;
+            var doctypeInformation = document.body.parentNode.previousSibling;
+            data.info.html = content;
+            opener.postMessage(data, "*");
+            console.log("tamper monkey: window location is ",window.location.href);
+        }
 
 
 
-    $( 'a' ).on( "focusin", function(evt) {
-    });
+        function AddCssAndHtml(){
+            if ($('#css-hover-popup').length <= 0) {
+                var s =  '';
+                s += '<style type="text/css"  id="css-hover-popup" >';
+                s += '.ui-draggable-handle {';
+                s += '    -ms-touch-action: none;';
+                s += '    touch-action: none;';
+                s += '}';
+                s += '#id32168 { ';
+                s += '    right: 10px; top: 10px; width: 150px; height: 70px; padding: 1.5em; background-color: #eaa;';
+                s += '    position: absolute; z-index: 2100;';
+                s += '}';
+                s += '</style>';
+                $(s).appendTo('head');
 
-    $( '#popup1' ).on( "focusout", function(evt) {
-    });
 
-    console.log( "tamper monkey document ready end" );
-});
+
+                var cnt = "https://citeproof.appspot.com/upload-receiver";
+                var popupUpScaffold = "<div id='bracket32168' style='position: relative;'><div id='id32168' >Drag_me_tamper</div></div>";
+                $('body').prepend(popupUpScaffold);  //next after <body;  most counter intuitive: stackoverflow.com/questions/5073016
+            }
+        }
+
+
+        $( document ).ready(function() {
+            console.log( "tamper monkey: document ready start" );
+
+
+            console.log("tamper monkey: window name is",window.name);
+            if (window.name==="browser_bridge_window") {
+                console.log("active");
+                AddCssAndHtml();
+                $( function() {
+                    $('#id32168').draggable({ cursor:"move" });
+                    //$('#id32168').onclick = TriggerIt;
+                    $('#id32168').click(TriggerIt);
+                });
+
+                $( function() {
+                    //TriggerIt();
+                });
+
+
+            }
+
+
+
+
+            console.log( "tamper monkey: document ready end" );
+        });
+
 
         console.log("isolated jQuery end");
     });
